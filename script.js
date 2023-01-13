e5bfb689656835c6b02f0e8891400a89653f5ac9
MIbmkz <s10242151@connect.np.edu.sg>
2023-01-13 16:04:58 +0800

    Added seperate header + footer for main page

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-//For form
-// Get the modal
-var modal = document.getElementById('id01');
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+$(function(){
+  $("#header").load("header.html"); 
+  $("#footer").load("footer.html"); 
+});
