15096219bffc2bec7ec74805de945b5d92c8e771
Ng Qian Zhe <s10243009@connect.np.edu.sg>
2023-01-15 15:37:18 +0800

    Update on the header and improvements to the button "Start Playing Here!"

---
+++ js/w3-include-header.js
@@ -0,0 +1,24 @@
+includeHeader();
+
+function includeHeader() {
+    var tag, index, a, file, xhttp;
+    tag = document.getElementsByTagName("*");
+    for (index = 0; index < tag.length; index++) {
+        if (tag[index].getAttribute("w3-include-header")) {
+            a = tag[index].cloneNode(false);
+            file = tag[index].getAttribute("w3-include-header");
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (xhttp.readyState == 4 && xhttp.status == 200) {
+                    a.removeAttribute("w3-include-header");
+                    a.innerHTML = xhttp.responseText;
+                    tag[index].parentNode.replaceChild(a, tag[index]);
+                    includeHeader();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            return;
+        }
+    }
+}
