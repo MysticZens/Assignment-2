1f7d77d14e209042b42561f91c2b03bf578f0342
MIbmkz <s10242151@connect.np.edu.sg>
2023-02-04 09:47:59 +0800

    Improved functions and game Cps

--- js/Cps.js
+++ js/Cps.js
@@ -1,50 +1,48 @@
-const startBtn = document.getElementById("startBtn");
-const timer = document.getElementById("timer");
-const gameArea = document.getElementById("gameArea");
 const countdown = document.getElementById("countdown");
-const result = document.getElementById("result");
+const gameArea = document.getElementById("game-area");
+const startButton = document.getElementById("start-button");
+const score = document.getElementById("score");
 
 let clicks = 0;
-let count = 10;
+let timeLeft = 10;
 let intervalId;
+let gameStarted = false;
 
-startBtn.addEventListener("click", startCountdown);
-gameArea.addEventListener("click", countClicks);
-
-function startCountdown() {
-  startBtn.style.display = "none";
-  countDown(3, startGame);
-}
-
-function countDown(seconds, callback) {
-  let count = seconds;
-  countdown.innerHTML = count;
-  intervalId = setInterval(function() {
-    count--;
-    countdown.innerHTML = count;
-    if (count === 0) {
-      clearInterval(intervalId);
-      countdown.innerHTML = "";
-      callback();
-    }
-  }, 1000);
-}
-
-function startGame() {
-  intervalId = setInterval(updateTimer, 1000);
-}
-
-function updateTimer() {
-  count--;
-  timer.innerHTML = "Time left: " + count + " seconds";
-  if (count === 0) {
-    clearInterval(intervalId);
-    gameArea.removeEventListener("click", countClicks);
-    result.innerHTML = "Total clicks: " + clicks;
-  }
-}
-
-function countClicks() {
+gameArea.addEventListener("click", function() {
+  if (!gameStarted || timeLeft === 0) return;
   clicks++;
-  result.innerHTML = "Total clicks: " + clicks;
+  score.innerHTML = `Score: ${clicks}`;
+});
+
+startButton.addEventListener("click", function() {
+  startButton.style.display = "none";
+  countdown.innerHTML = "Time remaining: 3";
+  setTimeout(function() {
+    countdown.innerHTML = "Time remaining: 2";
+  }, 1000);
+  setTimeout(function() {
+    countdown.innerHTML = "Time remaining: 1";
+  }, 2000);
+  setTimeout(function() {
+    gameStarted = true;
+    countdown.innerHTML = `Time remaining: ${timeLeft}`;
+    intervalId = setInterval(function() {
+      timeLeft--;
+      countdown.innerHTML = `Time remaining: ${timeLeft}`;
+      if (timeLeft === 0) {
+        clearInterval(intervalId);
+        countdown.innerHTML = `Time's up! Final score: ${clicks} <br> <button id="play-again">Play Again</button>`;
+        document.getElementById("play-again").addEventListener("click", playAgain);
+      }
+    }, 1000);
+  }, 3000);
+});
+
+function playAgain() {
+  clicks = 0;
+  timeLeft = 10;
+  gameStarted = false;
+  score.innerHTML = "Score: 0";
+  countdown.innerHTML = `Time remaining: ${timeLeft}`;
+  startButton.style.display = "inline-block";
 }
